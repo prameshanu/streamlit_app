f800eb32a42a09e0775635e26ea27d1b83a6cf4a
Pramesh Anuragi <prameshwork1@gmail.com>
2025-01-12 01:10:16 +0530

    Update Chatbot_app.py

--- Chatbot_app.py
+++ Chatbot_app.py
@@ -1,22 +1,22 @@
 import streamlit as st
 ## Data Ingestion 
-from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import OllamaEmbeddings  # Replace with appropriate embedding
-from langchain_community.vectorstores import Chroma
-from langchain_community.vectorstores import FAISS
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain_community.llms import Ollama
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
-from langchain_community.retrievers import PineconeHybridSearchRetriever
-import sentence_transformers
-from langchain.chains import RetrievalQA
+# from langchain_community.document_loaders import TextLoader
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain_community.embeddings import OllamaEmbeddings  # Replace with appropriate embedding
+# from langchain_community.vectorstores import Chroma
+# from langchain_community.vectorstores import FAISS
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_core.output_parsers import StrOutputParser
+# from langchain_community.llms import Ollama
+# from langchain.chains.combine_documents import create_stuff_documents_chain
+# from langchain.chains import create_retrieval_chain
+# from langchain_community.retrievers import PineconeHybridSearchRetriever
+# import sentence_transformers
+# from langchain.chains import RetrievalQA
 
-from pinecone import Pinecone
-from pinecone import ServerlessSpec
-from pinecone_text.sparse import BM25Encoder
+# from pinecone import Pinecone
+# from pinecone import ServerlessSpec
+# from pinecone_text.sparse import BM25Encoder
 
 
 
