c838533a4017dacb16ca27a627dccd7043bba806
Pramesh Anuragi <prameshwork1@gmail.com>
2025-01-12 22:29:27 +0530

    Update chatbot_app_v3.py

--- chatbot_app_v3.py
+++ chatbot_app_v3.py
@@ -67,7 +67,7 @@ pine_cone_api_key = st.secrets["PINE_CONE_API_KEY"]
 claude_api_key = st.secrets["CLAUDE_API_KEY"]
 
 langchain_api_key = st.secrets["LANGCHAIN_API_KEY"]
-hugging_face_api_key = st.secrets["HUGGING_FACE_API_KEY"]
+# hugging_face_api_key = st.secrets["HUGGING_FACE_API_KEY"]
 
 
 
@@ -221,7 +221,7 @@ import requests
 
 hugging_face_api_key
 API_URL = "https://api-inference.huggingface.co/models/google/gemma-2-2b-it"
-headers = {"Authorization": f"Bearer {hugging_face_api_key}"}
+headers = {"Authorization": f"Bearer {claude_api_key}"}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
