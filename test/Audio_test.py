5de5797a6fee116d68a224ce1f9610a5bf9a38cf
Pramesh Anuragi <prameshwork1@gmail.com>
2025-01-25 01:02:25 +0400

    Update Audio_test.py

--- test/Audio_test.py
+++ test/Audio_test.py
@@ -14,28 +14,33 @@ def main():
 	st.sidebar.title("Select the Modality")
 	option = st.sidebar.selectbox(
 	    "How would you like to be interact?",
-	    ("Chat", "Audio"),
+	    ("Select","Chat", "Audio"),
 	)
 	st.title ("ANCIENT GREEK Q&A blue[CHATBOT] :sunglasses:")
-	st.write ("Hi There, click on the voice recorder to interact with me, How can I assist you today?")
-	recorded_audio = audio_recorder()
-	if recorded_audio:
-		audio_file = "audio.mp3"
-		with open(audio_file , "wb") as f:
-			f.write(recorded_audio)
-		# Open the audio file
-		with open(audio_file, "rb") as file:
-		    # Create a transcription of the audio file
-		    transcription = client.audio.transcriptions.create(
-		      file=(audio_file, file.read()), # Required audio file
-		      model="whisper-large-v3-turbo", # Required model to use for transcription
-		      prompt="Specify context or spelling",  # Optional
-		      response_format="json",  # Optional
-		      language="en",  # Optional
-		      temperature=0.0  # Optional
-		    )
-		    # Print the transcription text
-		    st.write("User:",transcription.text)
+	if option == "Audio":
+		st.write ("Hi There, click on the voice recorder to interact with me, How can I assist you today?")
+		recorded_audio = audio_recorder()
+		if recorded_audio:
+			audio_file = "audio.mp3"
+			with open(audio_file , "wb") as f:
+				f.write(recorded_audio)
+			# Open the audio file
+			with open(audio_file, "rb") as file:
+			    # Create a transcription of the audio file
+			    transcription = client.audio.transcriptions.create(
+			      file=(audio_file, file.read()), # Required audio file
+			      model="whisper-large-v3-turbo", # Required model to use for transcription
+			      prompt="Specify context or spelling",  # Optional
+			      response_format="json",  # Optional
+			      language="en",  # Optional
+			      temperature=0.0  # Optional
+			    )
+			    # Print the transcription text
+			    st.write("User:",transcription.text)
+	elif option == "Chat":
+		st.write("Wecome to text chatbot")
+	else:
+		st.write("You have not selected any option")
 
 #Function to transcribe audio to text 
 
