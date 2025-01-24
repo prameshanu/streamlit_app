97c254c51e11cd470b8ca22aff1be82e28a4e619
Pramesh Anuragi <prameshwork1@gmail.com>
2025-01-25 00:43:04 +0400

    Update Audio_test.py

--- test/Audio_test.py
+++ test/Audio_test.py
@@ -6,33 +6,37 @@ from groq import Groq
 groq_api_key = st.secrets["GROC_API_KEY"]
 
 
+# Initialize the Groq client
+client = Groq(api_key=groq_api_key)
+
+
 def main():
 	st.sidebar.title("API KEY CONFIGURATION")
 	st.title ("Audio Test")
 	st.write ("Hi There, click on the voice recorder to interact with me, How can I assist you today?")
-	st.write(groq_api_key)
 	recorded_audio = audio_recorder()
+	if recorded_audio:
+		audio_file = "audio.mp3"
+		with open(audio_file , "wb") as f:
+			f.write(recorded_audio)
+
+	# Open the audio file
+	with open(audio_file, "rb") as file:
+	    # Create a transcription of the audio file
+	    transcription = client.audio.transcriptions.create(
+	      file=(filename, file.read()), # Required audio file
+	      model="whisper-large-v3-turbo", # Required model to use for transcription
+	      prompt="Specify context or spelling",  # Optional
+	      response_format="json",  # Optional
+	      language="en",  # Optional
+	      temperature=0.0  # Optional
+	    )
+	    # Print the transcription text
+	    st.write(transcription.text)
 
 #Function to transcribe audio to text 
 
 if __name__ == "__main__" :
 	main()
 
-# # Initialize the Groq client
-# client = Groq()
 
-# # Specify the path to the audio file
-# filename = os.path.dirname(__file__) + "/sample_audio.m4a" # Replace with your audio file!
-
-# # Open the audio file
-# with open(filename, "rb") as file:
-#     # Create a translation of the audio file
-#     translation = client.audio.translations.create(
-#       file=(filename, file.read()), # Required audio file
-#       model="whisper-large-v3", # Required model to use for translation
-#       prompt="Specify context or spelling",  # Optional
-#       response_format="json",  # Optional
-#       temperature=0.0  # Optional
-    # )
-    # Print the translation text
-    # print(translation.text)
