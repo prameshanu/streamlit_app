49440f658380be93d3b4fa54533ccac570d5a36e
Pramesh Anuragi <prameshwork1@gmail.com>
2025-01-26 15:02:03 +0400

    Update claude_test.py

--- test/claude_test.py
+++ test/claude_test.py
@@ -10,7 +10,6 @@ st.markdown(
         width: 100%;
         z-index: 100;
         padding: 10px 0;
-        /* box-shadow: 0px 2px 5px rgba(0,0,0,0.1); */
         text-align: left;
     }
     .content {
