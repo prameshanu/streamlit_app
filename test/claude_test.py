8200e9b5d80f740f1307da43425f0e306f6cff95
Pramesh Anuragi <prameshwork1@gmail.com>
2025-01-26 23:50:06 +0400

    Update claude_test.py

--- test/claude_test.py
+++ test/claude_test.py
@@ -55,7 +55,42 @@ def example():
                 label_visibility="collapsed",
                 placeholder="Type your message...",
             )
-
+                
+    with stylable_container(
+        key="heading",
+        css_styles="""
+            {
+                position: fixed;
+                top: 0; 
+                width: 80%;
+                left: 10%; 
+                right: 0;
+                border: 1px solid rgba(49, 51, 63, 0.2);
+                border-radius: 0.5rem 0.5rem 0 0;
+                padding: 1em;
+                background-color: white;
+                z-index: 100;
+            }
+        """,
+    ):
+        # Apply custom CSS for full-width input
+        st.markdown(
+            """
+            <style>
+            .full-width-input .stTextInput > div > div {
+                width: 100%; /* Slightly smaller width */
+                margin: 0 auto; /* Center it with equal margins on both sides */
+                
+            }
+            </style>
+            """,
+            unsafe_allow_html=True,
+        )
+    
+        # Wrap the text input in a class to target it
+        with st.container():
+            title = "ANCIENT GREEK Q&A CHATBOT"
+            st.title (f""":blue[{title}] """)
 
 
 
