eaca22cdd0b68d7fe9a14f28fea6b0d0c8a55ed7
Pramesh Anuragi <prameshwork1@gmail.com>
2025-01-26 14:51:03 +0400

    Update claude_test.py

--- test/claude_test.py
+++ test/claude_test.py
@@ -8,7 +8,6 @@ st.markdown(
         position: fixed;
         top: 0;
         width: 100%;
-        /* background-color: white; */
         z-index: 100;
         padding: 10px 0;
         box-shadow: 0px 2px 5px rgba(0,0,0,0.1);
