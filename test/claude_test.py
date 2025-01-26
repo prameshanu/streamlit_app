da4053b51c8eab0368e4aa605676b2117fdd4246
Pramesh Anuragi <prameshwork1@gmail.com>
2025-01-27 01:32:03 +0400

    Update claude_test.py

--- test/claude_test.py
+++ test/claude_test.py
@@ -160,7 +160,7 @@ def example():
 				tts(welcome_text,'en')
 				transcription = audio_processing()
 				# transcription= audio_to_text("audio.mp3")
-				st.write("User:",transcription.text)
+				# st.write("User:",transcription.text)
 				
 				if transcription:
 					query = transcription.text
