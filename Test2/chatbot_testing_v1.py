d6ef80799720a435a98a3cd29f3a3fd903c1c58c
Pramesh Anuragi <prameshwork1@gmail.com>
2025-02-16 12:22:32 +0400

    Update chatbot_testing_v1.py

--- Test2/chatbot_testing_v1.py
+++ Test2/chatbot_testing_v1.py
@@ -22,3 +22,15 @@ if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
+        
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
