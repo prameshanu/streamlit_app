e3878d67068581fa72345dd2c2b782ef6514b4d5
Pramesh Anuragi <prameshwork1@gmail.com>
2025-02-16 12:04:53 +0400

    Update chatbot_testing_v1.py

--- Test2/chatbot_testing_v1.py
+++ Test2/chatbot_testing_v1.py
@@ -1,11 +1,6 @@
-from pinecone import Pinecone
+
 import streamlit as st
 from groq import Groq
-
-pine_cone_api_key = st.secrets["PINE_CONE_API_KEY"]
-pc = Pinecone(api_key=pine_cone_api_key)
-# List available indexes to verify connectivity
-st.write(pc.list_indexes().names())
 
 prompt = st.chat_input("Say something")
 groq_api_key = st.secrets["GROC_API_KEY"]
