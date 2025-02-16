cbbbcb69184a7c929659d447fafe9d8ef7004501
Pramesh Anuragi <prameshwork1@gmail.com>
2025-02-16 12:02:06 +0400

    Update chatbot_testing_v1.py

--- Test2/chatbot_testing_v1.py
+++ Test2/chatbot_testing_v1.py
@@ -1,9 +1,42 @@
 from pinecone import Pinecone
 import streamlit as st
+from groq import Groq
+
 pine_cone_api_key = st.secrets["PINE_CONE_API_KEY"]
 pc = Pinecone(api_key=pine_cone_api_key)
 # List available indexes to verify connectivity
 st.write(pc.list_indexes().names())
 
 prompt = st.chat_input("Say something")
+groq_api_key = st.secrets["GROC_API_KEY"]
 
+# Initialize the Groq client
+client = Groq(api_key=groq_api_key)
+
+
+
+st.title("ChatGPT-like clone")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
