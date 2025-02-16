da32a06b509336ec854704efe078a1019cc35b20
Pramesh Anuragi <prameshwork1@gmail.com>
2025-02-16 12:28:39 +0400

    Update chatbot_testing_v1.py

--- Test2/chatbot_testing_v1.py
+++ Test2/chatbot_testing_v1.py
@@ -33,7 +33,7 @@ if prompt := st.chat_input("What is up?"):
 			}
             ],
         )
-        st.write(stream.choices[0].message.content)
+        response = stream.choices[0].message.content)
         # response = stream.choices[0].message.content
-        # response = st.write_stream(stream)
-    # st.session_state.messages.append({"role": "assistant", "content": response})
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
