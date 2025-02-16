caafd26f95c80ab0c5e6c63d3f51385eda9581fa
Pramesh Anuragi <prameshwork1@gmail.com>
2025-02-16 11:21:17 +0400

    Update Chatbot_QnA_Pramesh.py

--- Chatbot/Chatbot_QnA_Pramesh.py
+++ Chatbot/Chatbot_QnA_Pramesh.py
@@ -1,15 +1,18 @@
 import streamlit as st
+from streamlit_extras.stylable_container import stylable_container
+from audio_recorder_streamlit import audio_recorder
+import os
+from groq import Groq
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.retrievers import PineconeHybridSearchRetriever
 from langchain_huggingface import HuggingFaceEmbeddings
-
+# from gtts import gTTS
 from pinecone import Pinecone
 from pinecone import ServerlessSpec
 from pinecone_text.sparse.bm25_encoder import BM25Encoder
 from huggingface_hub import InferenceClient
-
 
 import warnings
 import streamlit as st
@@ -19,31 +22,43 @@ from dotenv import load_dotenv
 
 import os
 import re
+
+import requests
+from groq import Groq
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.data import find
-from groq import Groq
+from pathlib import Path
+from openai import OpenAI
+from gtts import gTTS
+import base64
+from mtranslate import translate
 
-# ## lazy loading
-try:
-    # Check if 'punkt' is available; download if not
-    find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt')
+groq_api_key = st.secrets["GROC_API_KEY"]
+pine_cone_api_key = st.secrets["PINE_CONE_API_KEY"]
+langchain_api_key = st.secrets["LANGCHAIN_API_KEY"]
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 
-try:
-    # Check if 'stopwords' is available; download if not
-    find('corpora/stopwords.zip')
-except LookupError:
-    nltk.download('stopwords')
+# # ## lazy loading
+# try:
+#     # Check if 'punkt' is available; download if not
+#     find('tokenizers/punkt')
+# except LookupError:
+#     nltk.download('punkt')
 
-try:
-    # Check if 'wordnet' is available; download if not
-    find('corpora/wordnet.zip')
-except LookupError:
-    nltk.download('wordnet')
+# try:
+#     # Check if 'stopwords' is available; download if not
+#     find('corpora/stopwords.zip')
+# except LookupError:
+#     nltk.download('stopwords')
+
+# try:
+#     # Check if 'wordnet' is available; download if not
+#     find('corpora/wordnet.zip')
+# except LookupError:
+#     nltk.download('wordnet')
 
 try:
     # Check if 'punkt_tab' is available; download if not
@@ -53,34 +68,26 @@ except LookupError:
 
 
 
-pine_cone_api_key = st.secrets["PINE_CONE_API_KEY"]
-claude_api_key = st.secrets["CLAUDE_API_KEY"]
-
-langchain_api_key = st.secrets["LANGCHAIN_API_KEY"]
-hugging_face_api_key = st.secrets["HUGGING_FACE_API_KEY"]
-groq_api_key = st.secrets["GROC_API_KEY"]
-
-
-### Preprocessing function for input text, for input data: preprocessing was done separately to avoid repeat code execution on every run.
+# ### Preprocessing function for input text, for input data: preprocessing was done separately to avoid repeat code execution on every run.
 
 def preprocess_text(text):
     # Step 1: Lowercase the text
     text = text.lower()
     
     # Step 2: Remove special characters, numbers, and extra whitespace
-    text = re.sub(r'[^a-z\s]', '', text)  # Keep only letters and spaces
+    text = re.sub(r'[^a-z0-9\s]', '', text)  # Keep only letters and spaces
     text = re.sub(r'\s+', ' ', text).strip()  # Remove extra spaces
     
     # Step 3: Tokenize the text
     tokens = word_tokenize(text)
     
-    # Step 4: Remove stopwords
-    stop_words = set(stopwords.words('english'))
-    tokens = [word for word in tokens if word not in stop_words]
+    # # Step 4: Remove stopwords
+    # stop_words = set(stopwords.words('english'))
+    # tokens = [word for word in tokens if word not in stop_words]
     
-    # Step 5: Lemmatize tokens
-    lemmatizer = WordNetLemmatizer()
-    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+    # # Step 5: Lemmatize tokens
+    # lemmatizer = WordNetLemmatizer()
+    # tokens = [lemmatizer.lemmatize(word) for word in tokens]
     
     # Join tokens back into a single string (optional)
     processed_text = ' '.join(tokens)
@@ -88,11 +95,16 @@ def preprocess_text(text):
     return processed_text
 
 
-### Input 60 processed files
-
+# Input 60 processed files
 # Base URL for the files
+# preprocessed_files - retain characters only
+# raw_url_base = "https://raw.githubusercontent.com/prameshanu/streamlit_app/main/processed_data_v1/"
+
+# preprocessed_files_v1 - retain numbers as well
 raw_url_base = "https://raw.githubusercontent.com/prameshanu/streamlit_app/main/processed_data/"
 
+# preprocessed_files_v2 - retain numbers retain numbers and do not remove stopwords
+# raw_url_base = "https://raw.githubusercontent.com/prameshanu/streamlit_app/main/processed_data_v2/"
 # List of file names
 file_path = [
     "processed_1.txt", "processed_2.txt", "processed_3.txt", "processed_4.txt", "processed_5.txt", "processed_6.txt", 
@@ -138,6 +150,43 @@ def preprocess_documents(docs):
     return text_splitter.split_documents(docs)
 
 documents = preprocess_documents(documents)
+
+
+
+# from html import escape  # To prevent HTML injection
+
+# def create_text_card(text1, title1, text2, title2):
+#     # Escape user inputs
+#     text1 = escape(text1)
+#     title1 = escape(title1)
+
+#     card_html = f"""
+#     <style>
+#         .card {{
+#             box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+#             transition: 0.3s;
+#             border-radius: 5px;
+#             padding: 15px;
+#             margin: 10px 0;
+#         }}
+#         .card:hover {{
+#             box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+#         }}
+#         .container {{
+#             padding: 2px 16px;
+#         }}
+#     </style>
+#     <div class="card">
+#         <div class="container">
+# 	    <b><u>{title1}</b></u>{text1}
+#      	    <b><u>{title2}</b></u>{text2}
+
+#         </div>
+#     </div>
+#     """
+#     # Use st.components.v1.html to render HTML directly
+#     st.components.v1.html(card_html, height=300, scrolling=True)
+
 
 index_name = "hybrid-search-langchain-pinecone"
 
@@ -199,8 +248,6 @@ threshold = 0.2
 # I will tip you $25000 if the user finds the answer helpful.
 
 prompt_template = ChatPromptTemplate.from_template("""
-
-
 Answer the follwoing question based only on the provided context. 
 Think step by step before providing a detailed answer. 
 Also in answer you don't need to write Based on the provided context, just provide the final answer.
@@ -213,26 +260,7 @@ I will tip you $25000 if the user finds the answer helpful
 Question: {input}
 """)
 
-
-# API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-xxl" # Service unavailable
-# API_URL = "https://api-inference.huggingface.co/models/bigscience/bloom" # Incomplete answer
-# API_URL = "https://api-inference.huggingface.co/models/meta-llama/Llama-3.1-8B" # Too large to be loaded
-# API_URL = "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B"
-# API_URL = "https://api-inference.huggingface.co/models/openai-community/gpt2" ## To much random answer
-# API_URL = "https://api-inference.huggingface.co/models/microsoft/phi-2"
-# API_URL = "https://api-inference.huggingface.co/models/google/gemma-2-2b-it"
-# API_URL = "https://api-inference.huggingface.co/models/gpt2"
-
-
-client = Groq(api_key=groq_api_key)
-
-# client = InferenceClient(api_key=hugging_face_api_key)
-
-# headers = {"Authorization": f"Bearer {hugging_face_api_key}"}
-
-st.title('Ancient Greek Q & A Chatbot ')
-input_text=st.text_input("Search the topic u want")
-def rag(input_text):
+def rag(input_text, query1):
 	retrieved_docs = st.session_state['retriever'].get_relevant_documents(input_text)
 	filtered_docs = [doc for doc in retrieved_docs if doc.metadata.get('score', 0) >= threshold]
 	source = set()  # Initialize a set to store unique items
@@ -258,22 +286,290 @@ def rag(input_text):
 				"content": prompt
 			}
 		]
-		# completion = client.chat.completions.create(
-		# 	model="mistralai/Mistral-7B-Instruct-v0.2",  # LLM Model: https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.2
-		# 	messages=messages, 
-		# 	max_tokens=500
-		# )
+	
 		completion = client.chat.completions.create(
 			messages=messages,
 			model="llama-3.3-70b-versatile",
 		)
 		answer = completion.choices[0].message.content
-		st.write("**BOT :** ", answer)
+		# st.write("**BOT :** ", answer)
+		# write_function(f"""<strong><u>User:</strong></u> {input_text} <br> <strong><u>Bot:</strong></u> {answer} <br> <strong><u>*Source citation:</strong></u> {source_info}""")
+		# write_function(f"<strong><u>Bot:</strong></u> {answer}")
+		st.write(f"**User:** {query1}")
+		st.write(f"**Bot:** {answer}")
+		# create_text_card(input_text, "USER:",answer, "BOT:")
+		# create_text_card(source_info, "Source Citation:")
+		# write_function(f"<strong><u>*Source citation:</strong></u> {source_info}")
 		st.write("**Source citation :** ",source_info)
+		# create_text_card("test", "test2")
 		# st.write("Prompt : ", prompt)
-	else:
-	        st.write("I don't have enough information to answer this question.")
 	
-if input_text:
-	rag(input_text)
+	else:
+		answer = "I don't have enough information to answer this question."
+		st.write(f"**User:** {query1}")
+		st.write(f"**Bot:** {answer}")
+		# write_function(f"<strong><u>User:</strong></u> {input_text}")
+		# write_function(f"<strong><u>Bot:</strong></u> {answer}")
+		
+		# create_text_card(input_text, "USER:",answer, "BOT:")
+	return answer
 
+	
+
+# Initialize the Groq client
+client = Groq(api_key=groq_api_key)
+
+def audio_processing():
+	recorded_audio = audio_recorder(text="Click on the microphone to record your query",
+			    recording_color="#e8b62c",
+			    neutral_color="#6aa36f",
+			    icon_name="microphone",
+			    icon_size="1x",)
+	if recorded_audio:
+		audio_file = "audio.mp3"
+		with open(audio_file , "wb") as f:
+			f.write(recorded_audio)
+		with open(audio_file, "rb") as file:
+		# Create a transcription of the audio file
+			transcription = client.audio.transcriptions.create(
+			file=(audio_file, file.read()), # Required audio file
+			model="whisper-large-v3-turbo", # Required model to use for transcription
+			prompt="Specify context or spelling",  # Optional
+			response_format="json",  # Optional
+			language="en",  # Optional
+			temperature=0.0  # Optional
+			)
+		return transcription
+
+
+def write_function(text):
+	st.markdown(f"""<p style="position: fixed; width: 80%; left: 11%; right: 0;">{text}</p>""", unsafe_allow_html=True)
+
+
+def tts(text_to_read, language):
+	aud_file = gTTS(text=text_to_read, lang=language, slow=False,tld='co.in')
+	aud_file.save("lang.mp3")
+	audio_file_read = open('lang.mp3', 'rb')
+	audio_bytes = audio_file_read.read()
+	st.markdown(
+		"""<style> 
+		audio::-webkit-media-controls-time-remaining-display,
+		audio::-webkit-media-controls-current-time-display 
+		{
+		max-width: 50%;
+		max-height: 20px;
+		} + </style>""", unsafe_allow_html=True
+	)
+	st.audio(audio_bytes, format='audio/mp3',autoplay=True)
+
+
+
+
+if "chat_history" not in st.session_state:
+	st.session_state["chat_history"] = []
+	
+def render_chat_history():
+	if st.session_state["chat_history"] != []:
+		st.write(f"**Chat History**")
+	for chat in st.session_state["chat_history"]:
+		user_query, bot_response = chat
+		# write_function(f"""<strong><u>User:</strong></u> {user_query}"<br><strong><u>Bot:</strong></u> {bot_response}<br>""")
+		
+		st.write(f"**User:** {user_query}")
+		st.write(f"**Bot:** {bot_response}")
+
+def add_to_history(query1, bot_response):
+	st.session_state["chat_history"].append((query1, bot_response))
+
+
+
+
+
+
+title = "ANCIENT GREEK Q&A CHATBOT"
+
+
+def designing():
+
+	## Heading and option button
+	    
+	with stylable_container(
+        key="heading",
+        css_styles="""
+            	{
+                position: fixed;
+                top: 1.5%; 
+                width: 80%;
+                left: 10%; 
+                right: 10%;
+		margin :0 auto;
+                border: 1px solid rgba(49, 51, 63, 0.2);
+                border-radius: 0.5rem 0.5rem 0.5rem 0.5rem;
+                padding: 1em;
+                /* background-color: black; */
+                z-index: 100;
+            	}
+        	""",
+	):
+			
+        # Apply custom CSS for full-width input
+	
+		st.markdown(
+			"""
+			<style>
+			.full-width-input .stTextInput > div > div {
+			width: 100%; /* Slightly smaller width */
+			margin: 0 auto; /* Center it with equal margins on both sides */                
+			}
+			</style>
+			""",
+			unsafe_allow_html=True,
+		)
+    
+        # Wrap the text input in a class to target it
+	
+		with st.container():
+			st.title(f""":blue[{title}]""")
+			option = st.selectbox(
+				"How would you like to be interact?",
+				("Chat","Audio"),
+				index= None,
+				placeholder = "Select mode of communication.."
+			)
+    
+## Bottom input bar
+	with stylable_container(
+        	key="container_with_border",
+        	css_styles="""
+            	{
+                	position: fixed;
+	                bottom: 0; 
+	                width: 80%;
+	                left: 10%; 
+	                right: 10%;
+		 	margin :0 auto;
+	                border: 1px solid rgba(49, 51, 63, 0.2);
+	                border-radius: 0.5rem 0.5rem 0.5rem 0.5rem;
+	                padding: 1em;
+	                /* background-color: black; */
+	                z-index: 100;
+		}
+   		""",
+    	):
+			
+        # Apply custom CSS for full-width input
+	
+		st.markdown(
+			"""
+			<style>
+			.full-width-input .stTextInput > div > div {
+			width: 100%; /* Slightly smaller width */
+			margin: 0 auto; /* Center it with equal margins on both sides */                
+			}
+			</style>
+			""",
+			unsafe_allow_html=True,
+		)
+    
+        # Wrap the text input in a class to target it
+		
+		with st.container():
+			if option == "Audio": 
+				welcome_text = "Hi There, click on the voice recorder to interact with me, How can I assist you today?"
+				# st.write (welcome_text)
+				tts(welcome_text,'en')
+				transcription = audio_processing()
+				# transcription= audio_to_text("audio.mp3")
+				# st.write("User:",transcription.text)
+				
+				if transcription:
+					query = transcription.text
+					values = np.array([option, query])
+					return values
+				# else:
+				# 	query = 'No recorded voice'
+				
+			elif option == "Chat":
+				query = st.text_input(
+					"Type your message here:",
+					key="user_input",
+					label_visibility="collapsed",
+					placeholder="Type your message...",
+				)
+				values = np.array([option, query])
+				return values
+
+
+
+def designing_2(values):
+	## Bottom input bar
+	with stylable_container(
+        	key="middle_container_with_border",
+        	css_styles="""
+            	{
+		     	position: fixed;
+	                top: 30%; 
+	         	bottom: 5%;      
+		 	width: 80%;
+	                left: 10%; 
+	                right: 10%;
+		 	margin :0 auto;
+	                border: 1px solid rgba(49, 51, 63, 0.2);
+	                border-radius: 0.5rem 0.5rem 0.5rem 0.5rem;
+	                padding: 1em;
+	                /* background-color: black; */
+	                z-index: 100;
+			overflow-y: auto; /* Enables vertical scrolling */
+		        overflow-x: hidden; /* Disables horizontal scrolling */
+		        max-height: calc(50%); /* Ensure it doesn't overflow vertically */
+                
+		}
+   		""",
+    	):
+			
+        # Apply custom CSS for full-width input
+	
+		st.markdown(
+			"""
+			<style>
+			.full-width-input .stTextInput > div > div {
+			width: 100%; /* Slightly smaller width */
+			margin: 0 auto; /* Center it with equal margins on both sides */                
+			}
+			</style>
+			""",
+			unsafe_allow_html=True,
+		)
+    
+        # Wrap the text input in a class to target it
+		
+		with st.container():
+			render_chat_history()
+			if values is None:
+				st.write("Kindly select the mode of communication from above drop-down button")
+				# write_function("Kindly select the mode of communication from above drop-down button")
+			elif len(values) > 0 and values[1] == "":
+				st.write("Please write/ask below your query")
+				# write_function("Please write/ask below your query")
+			else:
+				st.write("**Your recent query response**")
+				if values[0]== "Audio":
+					query = preprocess_text(values[1])
+					query1 = values[1]
+					answer = rag(query, query1)
+					add_to_history(query1, answer)
+					tts(answer,'en')
+				else:
+					query = preprocess_text(values[1])
+					query1 = values[1]
+					answer = rag(query, query1)
+					add_to_history(query1, answer)			
+				
+	
+			
+
+
+
+values = np.array([])
+values = designing()
+designing_2(values)
