b669bb8bd449755050387f9a27422868639e4620
Pramesh Anuragi <prameshwork1@gmail.com>
2025-02-16 11:20:17 +0400

    Update Chatbot_QnA_Pramesh.py

--- Chatbot/Chatbot_QnA_Pramesh.py
+++ Chatbot/Chatbot_QnA_Pramesh.py
@@ -24,7 +24,6 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.data import find
-import requests
 from groq import Groq
 
 # ## lazy loading
